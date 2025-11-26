317a344660adeed281dab8423829248638c0f181
Simon Holmes <simon@humancentricengineering.com>
2025-11-26 13:01:39 +0000

    fix: remove image from middle of homepage

--- src/endpoints/seed/index.ts
+++ src/endpoints/seed/index.ts
@@ -221,7 +221,7 @@ export const seed = async ({
     payload.create({
       collection: 'pages',
       depth: 0,
-      data: home({ heroImage: imageHomeDoc, metaImage: image2Doc }),
+      data: home({ heroImage: imageHomeDoc }),
     }),
     payload.create({
       collection: 'pages',
