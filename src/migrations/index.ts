a02ec4853cf9e162cec46975b15a8a6653ddc984
Simon Holmes <simon@humancentricengineering.com>
2025-12-10 11:54:47 +0000

    Add database migration for network content

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251205_160143 from './20251205_160143';
+import * as migration_20251210_115406 from './20251210_115406';
 
 export const migrations = [
   {
     up: migration_20251205_160143.up,
     down: migration_20251205_160143.down,
-    name: '20251205_160143'
+    name: '20251205_160143',
+  },
+  {
+    up: migration_20251210_115406.up,
+    down: migration_20251210_115406.down,
+    name: '20251210_115406'
   },
 ];
